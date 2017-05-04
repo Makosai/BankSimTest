5bd104972f143fe36e6735ce5a73c10726efe074
Yosvany Reina <YosvanyReina@gmail.com>
2017-05-04 18:10:45 -0400

    Init Proj.

---
+++ src/bankSimTesting/TransactionType.java
@@ -0,0 +1,10 @@
+package bankSimTesting;
+
+/** A list of the possible types of transactions in the banking simulation.
+ * @author wpollock
+ *
+ */
+public enum TransactionType {
+    DEPOSIT, WITHDRAWAL, INTEREST, CHECK, FEE, PENALTY, ADJUSTMENT;
+}
+
